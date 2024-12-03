2d5a4968e144554b14683a3c0eabce7c8094e57c
RIFFXD <arifffauzan383@gmail.com>
2024-12-03 08:45:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,6 @@
-// Mengambil elemen tombol dan konten yang relevan
-const showStoryButton = document.getElementById('showStory');
-const storyContent = document.getElementById('storyContent');
-
-const showAbstractButton = document.getElementById('showAbstract');
-const abstractContent = document.getElementById('abstractContent');
-
-const showEvaluationButton = document.getElementById('showEvaluation');
-const evaluationContent = document.getElementById('evaluationContent');
-
-const showAnalysisButton = document.getElementById('showAnalysis');
-const analysisContent = document.getElementById('evaluationContent');
-
-// Fungsi untuk menampilkan konten cerpen
-showStoryButton.addEventListener('click', () => {
-    storyContent.classList.toggle('hidden');
-    showStoryButton.textContent = storyContent.classList.contains('hidden') ? 'Lihat Cerpen' : 'Sembunyikan Cerpen';
-});
-
-// Fungsi untuk menampilkan konten abstrak
-showAbstractButton.addEventListener('click', () => {
-    abstractContent.classList.toggle('hidden');
-    showAbstractButton.textContent = abstractContent.classList.contains('hidden') ? 'Lihat Abstrak' : 'Sembunyikan Abstrak';
-});
-
-// Fungsi untuk menampilkan konten evaluasi
-showEvaluationButton.addEventListener('click', () => {
-    evaluationContent.classList.toggle('hidden');
-    showEvaluationButton.textContent = evaluationContent.classList.contains('hidden') ? 'Lihat Evaluasi & Revolusi' : 'Sembunyikan Evaluasi & Revolusi';
-});
-
-// Fungsi untuk menampilkan konten analisis
-showAnalysisButton.addEventListener('click', () => {
-    analysisContent.classList.toggle('hidden');
-    showAnalysisButton.textContent = analysisContent.classList.contains('hidden') ? 'Lihat Unsur Intrinsik dan Ekstrinsik' : 'Sembunyikan Unsur Intrinsik dan Ekstrinsik';
+// Menunggu tombol diklik dan menampilkan cerita
+document.getElementById("showStory").addEventListener("click", function() {
+    // Menampilkan elemen cerita dengan menambahkan kelas 'show'
+    var story = document.getElementById("story");
+    story.classList.toggle("show"); // Toggle efek muncul dan sembunyi
 });
