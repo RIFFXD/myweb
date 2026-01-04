689e0da39314ac5117bfa42dc614fc4cde491d89
RIFFXD <arifffauzan383@gmail.com>
2026-01-04 19:15:28 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Menunggu tombol diklik dan menampilkan cerita
-document.getElementById("showStory").addEventListener("click", function() {
-    // Menampilkan elemen cerita dengan menambahkan kelas 'show'
-    var story = document.getElementById("story");
-    story.classList.toggle("show"); // Toggle efek muncul dan sembunyi
-});
