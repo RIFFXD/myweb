499a7c390aea7773cdb5393f6b70bc17b57d5db8
RIFFXD <arifffauzan383@gmail.com>
2026-01-04 19:19:58 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+// ATLANTIC
+define('ATLANTIC_API_KEY', 'apikey');
+define('ATLANTIC_BASE_URL', 'https://atlantich2h.com');
+
+// TELEGRAM
+define('BOT_TOKEN', 'tokenbot');
+define('CHAT_ID', 'id-admin');
