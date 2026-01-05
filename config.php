9da02b7968d929f0762af4ed1a716b3cca2a91df
RIFFXD <arifffauzan383@gmail.com>
2026-01-05 08:52:19 +0700

    Update Telegram bot token and chat ID

--- config.php
+++ config.php
@@ -4,5 +4,5 @@ define('ATLANTIC_API_KEY', 'apikey');
 define('ATLANTIC_BASE_URL', 'https://atlantich2h.com');
 
 // TELEGRAM
-define('BOT_TOKEN', 'tokenbot');
-define('CHAT_ID', 'id-admin');
+define('BOT_TOKEN', '8217953547:AAFXfKjwhMQwH_HhqtQBb1zjp3-WR-TJ3yg');
+define('CHAT_ID', '8373014782');
